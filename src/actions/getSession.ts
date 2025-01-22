e73230d1c58247b05c16ecc25eeeb26b13e29345
leVert <selmamathis15@gmail.com>
2025-01-22 17:07:24 +0300

    baseUrl changed

--- src/actions/getSession.ts
+++ src/actions/getSession.ts
@@ -1,7 +1,8 @@
 
 
 export async function getCurrentUser(req?: any) {
-  const url = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/auth/session';
+  const baseURL='/'
+  const url = `${baseURL}/api/auth/session`
   const options: RequestInit = {
     method: 'GET',
     credentials: 'include',
