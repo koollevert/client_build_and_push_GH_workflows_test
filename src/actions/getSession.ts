79f9b4180599fdaf7ce29e6d3d71d0cd9ff9a13e
leVert <selmamathis15@gmail.com>
2025-01-22 16:51:52 +0300

    updated getCurrentUser

--- src/actions/getSession.ts
+++ src/actions/getSession.ts
@@ -1,7 +1,7 @@
 'use server';
 
 export async function getCurrentUser(req?: any) {
-  const url = '/api/auth/session';
+  const url = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/auth/session';
   const options: RequestInit = {
     method: 'GET',
     credentials: 'include',
@@ -11,13 +11,18 @@ export async function getCurrentUser(req?: any) {
     options.headers = req.headers;
   }
 
-  const response = await fetch(url, options);
+  try {
+    const response = await fetch(url, options);
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch current user');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch current user: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log('Current User:', data.currentUser); // Log the current user
+    return data.currentUser || null; // Return the user if present, otherwise null
+  } catch (error) {
+    console.error('Error fetching current user:', error);
+    throw error;
   }
-
-  const data = await response.json();
-  console.log('Current User:', data.currentUser); // Log the current user
-  return data.currentUser || null; // Return the user if present, otherwise null
 }
